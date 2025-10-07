2a992582fd1c47382e93256f59560b980ecd725c
thomasdinh <work.dnstung@gmail.com>
2025-10-07 23:08:21 +0700

    WIP exercise 3

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import {sample} from "../../utils";
 import {WORDS} from "../../data";
 import GuessInput from "./Input/GuessInput";
 import {GuessList} from "./Render/GuessList";
+import {Guess} from "./Render/Guess";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,19 +12,32 @@ const answer = sample(WORDS);
 console.info({answer});
 
 function Game() {
-    const [userGuess, setUserGuess] = React.useState([]);
+    const initializeUserGuess = [];
+    for (let i = 1; i <= 5; i += 1) {
+        initializeUserGuess.push(
+            {
+                id: `Guess-${i}`,
+                content: "",
+            },
+        );
+    }
+    ;
+
+    const [userGuess, setUserGuess] = React.useState(initializeUserGuess);
 
     function submitGuess(value) {
         console.log({guess: value});
-        const nextGuess = [...userGuess, {
-            id: crypto.randomUUID(),
-            content: value,
-        }];
+
+        const nextGuess = [...userGuess];
+        nextGuess[nextGuess.findIndex(i => i.content === "")].content = value;
+
+        console.log("nextGuess", nextGuess);
 
         setUserGuess(nextGuess);
     }
 
     return <>
+        <Guess userGuess={userGuess}/>
         <GuessList userGuess={userGuess}/>
         <GuessInput submitGuess={submitGuess}/>
     </>;
